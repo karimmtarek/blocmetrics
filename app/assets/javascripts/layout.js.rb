4010b5984431b86d18ab5bba17f284a1d037c2bb
Karim Tarek <karimmtarek@gmail.com>
2015-01-25 00:00:24 -0500

    js comment, user ctrl minor fix

--- app/assets/javascripts/layout.js.rb
+++ app/assets/javascripts/layout.js.rb
@@ -1,9 +1,9 @@
 require 'opal-jquery'
 Document.ready? do
-  puts Window.height
-  puts Window.width
-  Element['.something'].on(:click) do |event|
-    puts "clicked on: #{event.current_target}"
-    event.current_target.effect('fadeOut')
-  end
+  # puts Window.height
+  # puts Window.width
+  # Element['.something'].on(:click) do |event|
+  #   puts "clicked on: #{event.current_target}"
+  #   event.current_target.effect('fadeOut')
+  # end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
 
   def require_signin
     unless current_user
-      session[:intended_url] = request.url
-      redirect_to signin_path, alert: "Please sign-in first!"
+      # session[:intended_url] = request.url
+      redirect_to signin_path#, alert: "Please sign-in first!"
     end
   end
 
